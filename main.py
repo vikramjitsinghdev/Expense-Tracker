8d67038ed3f294f1f1a063371ff262f55ba0f5c3
Vikramjit Singh <vikramjit.singh.dev@gmail.com>
2026-08-15 23:01:47 -0300

    Index Creation and User Validation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 name = input("Enter your name: ")
 print("Hello", name)
+expenses = []
 total_spent = 0
+payment_methods = ["credit", "debit", "cash", "other"]
 total_budget = float(input("Enter your total budget: "))
 number_of_expenses = int(input("How many expenses do you want to enter? "))
 for i in range(1, number_of_expenses + 1):
@@ -17,8 +20,21 @@ for i in range(1, number_of_expenses + 1):
             print("Please enter a valid number.")
     category = input("What category? ")
     amount = int(input("How many items did you buy? "))
-    date = input("What date? (YYYY-MM-DD) ")
-    payment = input("How did you pay? ")
+    while True:
+        date = input("What date? (YYYY-MM-DD) ")
+
+        try:
+            datetime.strptime(date, "%Y-%m-%d")
+            break
+        except ValueError:
+            print("Invalid date. Please use YYYY-MM-DD.")
+    while True:
+        payment = input("How did you pay? ").lower()
+
+        if payment in payment_methods:
+            break
+        else:
+            print("Invalid payment method. Please enter credit, debit, cash, or other.")
     while amount < 1:
         print("Please enter an amount of at least 1.")
         amount = int(input("How many items did you buy? "))
@@ -35,3 +51,9 @@ for i in range(1, number_of_expenses + 1):
     expense.append(amount)
     expense.append(date)
     expense.append(payment)
+    expenses.append(expense)
+    print_expenses = input("Do you want to print all expenses? (yes/no) ").lower()
+    if print_expenses == "yes":
+        print("\nAll Expenses:")
+        for exp in expenses:
+            print(f"Expense {exp[0]}: {exp[1]} - ${exp[2]} in {exp[3]} on {exp[5]} via {exp[6]}\n")
